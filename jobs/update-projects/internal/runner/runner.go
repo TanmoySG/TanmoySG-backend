b5e0aa7ce07bd9d6de76750da6258d8c9239a4b9
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-06-17 01:32:14 +0530

    added error descriptions

--- jobs/update-projects/internal/runner/runner.go
+++ jobs/update-projects/internal/runner/runner.go
@@ -2,6 +2,7 @@ package runner
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/TanmoySG/TanmoySG-backend/jobs/update-projects/internal/common"
 	"github.com/TanmoySG/TanmoySG-backend/jobs/update-projects/internal/github"
@@ -40,19 +41,18 @@ func NewConfig(database, collection, username string) configurations {
 func (c client) Run(configurations configurations) error {
 	pinnedRepositories, err := c.githubClient.GetPinnedRepositories(configurations.username)
 	if err != nil {
-		return err
+		return fmt.Errorf("error getting pinned repositories: %s", err)
 	}
-
 	log.Infof("fetched pinned repositories for user [%s] from gitHub.  total repositories fetched [%d]", configurations.username, len(pinnedRepositories))
 
 	existingProjectsMap, err := c.wdbAdapter.GetData(configurations.wdbDatabase, configurations.wdbCollection)
 	if err != nil {
-		return err
+		return fmt.Errorf("error getting existing projects map: %s", err)
 	}
 
 	existingProjects, err := common.GetProjectsList(existingProjectsMap.Data)
 	if err != nil {
-		return err
+		return fmt.Errorf("error getting existing projects list: %s", err)
 	}
 
 	log.Infof("fetched existing projects. total projects fetched [%d]", len(existingProjects))
@@ -62,20 +62,20 @@ func (c client) Run(configurations configurations) error {
 		transformedProject := transform.Do(actualSequence, item, existingProjects)
 		log.Infof("transformed repo details to project [%s / %s], sequence [%d]", transformedProject.Name, transformedProject.Codelink, actualSequence)
 
-		var transformedProjectMap map[string]interface{}
 		transformedProjectByte, err := json.Marshal(transformedProject)
 		if err != nil {
-			return err
+			return fmt.Errorf("error marshalling transformedProject to bytes: %s", err)
 		}
 
+		var transformedProjectMap map[string]interface{}
 		err = json.Unmarshal(transformedProjectByte, &transformedProjectMap)
 		if err != nil {
-			return err
+			return fmt.Errorf("error unmarshalling transformedProject bytes to map: %s", err)
 		}
 
 		err = c.wdbAdapter.AddData(configurations.wdbDatabase, configurations.wdbCollection, transformedProjectMap)
 		if err != nil {
-			return err
+			return fmt.Errorf("error adding data to wdb-collection: %s", err)
 		}
 		log.Infof("inserted pinned repo as project [%s]", transformedProject.Name)
 	}
@@ -83,7 +83,7 @@ func (c client) Run(configurations configurations) error {
 	for _, existingProject := range existingProjects {
 		err = c.wdbAdapter.DeleteData(configurations.wdbDatabase, configurations.wdbCollection, "_id", existingProject.ID)
 		if err != nil {
-			return err
+			return fmt.Errorf("error deleting data from wdb-collection: %s", err)
 		}
 		log.Infof("deleted project [%s]", existingProject.Name)
 	}
